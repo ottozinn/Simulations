737a3dda7d3305b217c4b90bf8e2f395c21ff783
ottozinn <ottozinn@icloud.com>
2020-02-18 08:54:17 -0600

    Hi

--- carddeck.py
+++ carddeck.py
@@ -1,20 +1,26 @@
+import random
 deck = []
 suits = ["♠","♣","♥","♦"]
-
-    
-
 for i in range(4):
     for j in range(1,14):
         value = str(j)
-        if(j == 1):
-            value = "A"
-        elif(j == 11):
+        if (j == 11):
             value = "J"
-        elif(j == 12):
+        elif (j == 12):
             value = "Q"
-        elif(j == 13):
+        elif (j == 13):
             value = "K"
+        elif (j == 1):
+            value = "A"
         deck.append(f"{value}{suits[i]}")
-
-
+        
 print(deck)
+
+for i in range(len(deck)-1, 0, -1): 
+
+    j = random.randint(0, i + 1)  
+
+
+    deck[i], deck[j] = deck[j], deck[i]  
+
+print ("The shuffled deck is : " +  str(deck))
